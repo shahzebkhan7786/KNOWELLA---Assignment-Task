bdffbaa763e0efb6d2536714f3a323e420f65776
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-23 13:39:06 +0530

    Update weather.js

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -3,8 +3,19 @@ const axios = require("axios");
 exports.getWeather = async (lat, lon) => {
   const apiKey = process.env.WEATHER_API_KEY;
 
+  if (!apiKey) {
+    throw new Error("Missing API Key");
+  }
+
   const res = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather`,
+    {
+      params: {
+        lat,
+        lon,
+        appid: apiKey
+      }
+    }
   );
 
   return res.data;
