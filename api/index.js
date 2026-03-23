1f0b1204b526964422bcc77ff5a2c710951e42ca
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-23 13:46:20 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.get("/api/csv", async (req, res) => {
   try {
     const filePath = path.join(process.cwd(), "sample.csv");
-    const data = await processCSV(filePath, config);
+    const data = await processCSV(config);
     res.json(data);
   } catch (err) {
     console.error("CSV ERROR:", err);
